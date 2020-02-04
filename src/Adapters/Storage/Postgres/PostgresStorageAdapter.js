4240b398f0defb801481dad15b4bc800c8d3267d
Kain <evgeniy@macsoftex.com>
2020-02-04 08:33:23 +0300

    Fix

--- src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
+++ src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
@@ -283,7 +283,8 @@ const buildWhereClause = ({ schema, query, index }): WhereClause => {
       } else {
         if (fieldValue.$in) {
           name = transformDotFieldToComponents(fieldName).join('->');
-          patterns.push(`$${index} ?| $${index + 1}::array`);
+          console.log("Postgres:: fieldValue.$in: " + fieldValue.$in);
+          patterns.push(`$${index} ?| ARRAY$${index + 1}`);
           values.push(name, fieldValue.$in);
           index += 2;
         } else if (fieldValue.$regex) {
